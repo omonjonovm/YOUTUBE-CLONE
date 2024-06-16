8362c6ce064801726614d219d319f4431fafb634
omonjonovm <omonjonovmurodjon2@gmail.com>
2024-06-16 10:51:55 +0500

    add videos and , channel card

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,9 +4,11 @@ import { fetchFromAPI } from "../utils/fetchFromAPI";
 import { Videos, Sidebar } from "./";
 const Feed = () => {
  const [selectedCategory, setSelectedCategory] = useState('New')
+ const [videos,setVideos] = useState([])
 
   useEffect(() => {
     fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    .then((data) => setVideos(data.items))
   },[selectedCategory]);
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
@@ -16,11 +18,11 @@ const Feed = () => {
           Copyright 2024
         </Typography>
       </Box>
-      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+      <Box p={2} sx={{ overflowY: 'auto', height: '97vh', flex: 2 }}>
         <Typography variant='h4' fontWeight="bold" mb={2} sx={{ color: 'white', }}>
           {selectedCategory} <span style={{ color: '#F31503' }}>videos</span>
         </Typography>
-        <Videos videos={[]} />
+        <Videos videos={videos} />
       </Box>
     </Stack>
   )
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,8 +1,17 @@
+import { Box, Stack } from '@mui/material'
 import React from 'react'
+import {VideoCard,ChannelCard} from './'
 
-const Videos = () => {
+const Videos = ({videos}) => {
   return (
-    <div>Videos</div>
+    <Stack direction="row" flexWrap={"wrap"} justifyContent={'start'} gap={2} >
+     {videos.map((item,idx) => (
+      <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+      </Box>
+     ))}
+    </Stack>
   )
 }
 
