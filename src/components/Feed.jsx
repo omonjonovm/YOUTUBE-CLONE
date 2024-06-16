d97753bb98e90334f81cb28abf1b26e9bb1bd2e0
omonjonovm <omonjonovmurodjon2@gmail.com>
2024-06-16 12:28:51 +0500

    profil page and search

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,18 +3,31 @@ import { Box, Stack, Typography } from "@mui/material"
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 import { Videos, Sidebar } from "./";
 const Feed = () => {
- const [selectedCategory, setSelectedCategory] = useState('New')
- const [videos,setVideos] = useState([])
+  const [selectedCategory, setSelectedCategory] = useState('New')
+  const [videos, setVideos] = useState([])
 
   useEffect(() => {
     fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
-    .then((data) => setVideos(data.items))
-  },[selectedCategory]);
+      .then((data) => setVideos(data.items))
+  }, [selectedCategory]);
   return (
-    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Box sx={{ height: { sx: 'auto', md: '94vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
-        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}/>
-        <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff' }}>
+    <Stack
+      sx={{
+        flexDirection:
+          { sx: "column", md: "row" }
+      }}>
+      <Box
+        sx={{
+          height: { sx: 'auto', md: '94vh' },
+          borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 }
+        }}>
+        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+        <Typography className='copyright'
+          variant='body2'
+          sx={{
+            mt: 1.5,
+            color: '#fff'
+          }}>
           Copyright 2024
         </Typography>
       </Box>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,11 +3,21 @@ import { useNavigate } from "react-router-dom"
 import { Paper, IconButton } from "@mui/material"
 import { Search } from "@mui/icons-material"
 const SearchBar = () => {
+  const navigate = useNavigate()
+  const [searchTerm, setSearchTerm] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(searchTerm){
+      navigate(`/search/${searchTerm}`)
+      setSearchTerm('');
+    }
+  }
   return (
-    <Paper component="form" onSubmit={() => { }} sx={{ borderRadius: 20, border: '1px solid #e3e3e3', pl: 2, boxShadow: 'none', mr: { sm: 5 } }} >
+    <Paper component="form" onSubmit={handleSubmit} sx={{ borderRadius: 20, border: '1px solid #e3e3e3', pl: 2, boxShadow: 'none', mr: { sm: 5 } }} >
       <input type="text" className='search-bar'
         placeholder='Search...'
-        onChange={() => { }} />
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)} />
       <IconButton type='submit' sx={{
         p:'10px', color:'red'
       }}>
